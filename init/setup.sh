9c35a0d177bcc09fe96f5da245bb6dc78913e63c
yaowenxu <yaowen1996@hotmail.com>
2018-08-13 10:35:14 +0800

    modify init setup files

--- init/setup.sh
+++ init/setup.sh
@@ -7,9 +7,10 @@ echo "Env"
 # zsh
 # oh_my_zsh
 # tig : 字符模式下交互查看git项目
+# gitk : Git repository browser
 # jq : json文件格式化处理以及高亮显示
 # shellcheck : shell脚本静态检查工具，识别语法错误以及不规范的写法
 # fzf : 命令行下模糊搜索工具，能够交互式智能搜索并选取文件或者内容
 # script/scriptreplay : 终端录制
 
-# mac say, pbcopy/pbpaste 命令注意使用
+# mac say, pbcopy/pbpaste  : 语音和 Terminal 与 系统粘贴板 通信
