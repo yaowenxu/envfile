50d96455aecfd44918d5b0e5c706d11963c8d81b
Yaoxu <yaowenxu@users.noreply.github.com>
2018-09-06 21:47:13 +0800

    更新条目
    更新条目

--- init/setup.sh
+++ init/setup.sh
@@ -12,5 +12,8 @@ echo "Env"
 # shellcheck : shell脚本静态检查工具，识别语法错误以及不规范的写法
 # fzf : 命令行下模糊搜索工具，能够交互式智能搜索并选取文件或者内容
 # script/scriptreplay : 终端录制
+# cockpit
+# Jupyter
+# fish 
 
 # mac say, pbcopy/pbpaste  : 语音和 Terminal 与 系统粘贴板 通信
