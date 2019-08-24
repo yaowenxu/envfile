c2b6830f033577b55153a34b6f567af537e5291c
yaowenxu <yaowen1996@hotmail.com>
2019-08-24 11:04:44 +0800

    add some useful tools to the repo

--- init/setup.sh
+++ init/setup.sh
@@ -1,21 +1,35 @@
+### 
+# @Author: Yaowen Xu
+ # @Github: https://github.com/yaowenxu
+ # @Organization: 北航系统结构研究所
+ # @Date: 2019-08-17 17:07:58
+ # @LastEditTime: 2019-08-24 11:04:03
+ # @Description: 文件描述信息
+ ###
 #!/usr/bin/env sh
 echo "Env"
 
-# vim
-# tmux
-# ssh
-# zsh
-# oh_my_zsh
+# vim 经常使用的编辑器
+# tmux 可以保持多窗口多会话不中断
+# ssh ssh 连接器
+# zsh/fish 比较好用的shell
+# oh_my_zsh zsh shell 的主题
+# ag 更快速的递归搜索文件内容
 # tig : 字符模式下交互查看git项目
-# gitk : Git repository browser
+# gitk : Git repository browser 一个Git图形化浏览工具，mac 上显示分辨率不清晰
+# htop : 进程监控工具，更美观，更好用，可替代 top 命令；
+# glances : 比htop 有更多信息，但是没有 htop 快捷的命令，建议结合htop 一起使用。glances 还有web界面 比较易用。
 # jq : json文件格式化处理以及高亮显示
 # shellcheck : shell脚本静态检查工具，识别语法错误以及不规范的写法
 # fzf : 命令行下模糊搜索工具，能够交互式智能搜索并选取文件或者内容
+# axel - light command line download accelerator 多线程下载器，可以替代 wget 和 curl -O 
+# cloc 代码统计工具
 # script/scriptreplay : 终端录制
 # cockpit
 # Jupyter https://www.cnblogs.com/xuyaowen/p/fedora-jupyter.html
 # fish 
 # 设置 ctrl + alt + T 快捷打开 terminal 的快捷键
-# 安装 vnc 进行远程主机访问 https://www.cnblogs.com/xuyaowen/p/vnc-remote-desktop.html
+# 安装 vnc 进行远程主机访问 https://www.cnblogs.com/xuyaowen/p/vnc-remote-desktop.html 
+# multitail 一次可以监控很多日志文件，在同一个命令行中，具体使用说明可以参考 man手册 
 
 # mac say, pbcopy/pbpaste  : 语音和 Terminal 与 系统粘贴板 通信
